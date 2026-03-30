f0877e6b7c2dff5e57ba1b1d0f34f650dd06e7ba
Mr-Capy <capytest@proton.me>
2026-03-29 20:40:16 -0400

    Update model.py

--- model.py
+++ model.py
@@ -39,11 +39,11 @@ class bot:
             #generated history
             self.chat_history_ids = model.generate(
             bot_input_ids, 
-            max_length=35,
+            max_new_tokens=20,
             do_sample=True,
-            top_k=50,
-            top_p=0.95,
-            temperature=0.7, 
+            top_p=0.7,
+            temperature=0.7,
+            repetition_penalty=1.1,
             pad_token_id=tokenizer.eos_token_id
             ) 
 
